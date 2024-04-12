2779f26154f5ab57114691cad80b30251ebef257
Vitalii Kondrashov <52565293+evolver1337@users.noreply.github.com>
2024-04-12 15:24:13 +0300

    New Dog

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -12,12 +12,11 @@ public class AnimalsCage {
     private Animal animal;
 
     @Autowired
-    private final Timer timer;
+    private Timer timer;
 
-    public AnimalsCage(Timer timer) {
-        this.timer = timer;
+    public Timer getTimer() {
+        return timer;
     }
-
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -26,4 +25,5 @@ public class AnimalsCage {
         System.out.println(timer.getTime());
         System.out.println("________________________");
     }
+
 }
